5d7e13046833555eb37363e4010e883bf3a6822c
thomas <thomas@antmoves.com>
2020-02-08 16:44:46 +0200

    playing

--- src/js/main.js
+++ src/js/main.js
@@ -29,24 +29,24 @@ Promise.all([windowLoad, imagesLoadedP, window.preloaderDone]).then(() => {
 });
 
 const styleD = [
-  'background: #000;',
+  'background: #000',
   'color: white',
   'padding: 5px 20px',
   'font-weight: 400',
   'line-height: 15px',
 ].join(';');
 /* eslint-disable-next-line no-console */
-console.log('%c 🤔💭 2b || !2b', styleD);
+console.log('%c🤔💭 2b || !2b', styleD);
 
 const style = [
-  'background: #d33d27;',
+  'background: #d33d27',
   'color: white',
   'padding: 5px 20px',
-  'font-weight: 400',
+  'font-weight: 700',
   'line-height: 25px',
 ].join(';');
 /* eslint-disable-next-line no-console */
-console.log('%cMade by AntMoves | Explore more projects @ antmoves.com', style);
+console.log('%cAntmoves | Explore more projects @ antmoves.com', style);
 
 
 // const users = [{
